8092f006611666c3707f2d4a1f76f64f607d51b9
Ryan Griffin <ryangrif875@gmail.com>
2021-11-17 18:52:50 -0500

    updated fitness tips subpages

--- javascript.js
+++ javascript.js
@@ -1,11 +1,11 @@
 function button_press() {
-  var x = document.getElementById("drop-down_content");
-  var y = document.getElementById("button");
-  if (x.style.width === "250px") {
-    x.style.width = "0";
-    y.style.backgroundImage = "url(uf_buttons/hamburger.png)"
-  } else {
-    x.style.width = "250px";
-    y.style.backgroundImage = "url(uf_buttons/close.png)"
-  }
+	var x = document.getElementById("drop-down_content");
+	var y = document.getElementById("button");
+	if (x.style.width === "250px") {
+		x.style.width = "0";
+		y.style.backgroundImage = "url(uf_buttons/hamburger.png)"
+	} else {
+		x.style.width = "250px";
+		y.style.backgroundImage = "url(uf_buttons/close.png)"
+	}
 }
